fc5d25ece183b25f6e807b90ecaf17a5b1af2523
Haoxiang Gao <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-04-15 12:59:39 +0800

    Update crawler.py

--- proxypool/crawler.py
+++ proxypool/crawler.py
@@ -43,7 +43,29 @@ class Crawler(object, metaclass=ProxyMetaclass):
                     port = tr.find('td:nth-child(2)').text()
                     yield ':'.join([ip, port])
 
-
+    def crawl_ip3366(self):
+        for page in range(1, 4):
+            start_url = 'http://www.ip3366.net/free/?stype=1&page={}'.format(page)
+            html = get_page(start_url)
+            ip_address = re.compile('<tr>\s*<td>(.*?)</td>\s*<td>(.*?)</td>')
+            # \s * 匹配空格，起到换行作用
+            re_ip_address = ip_address.findall(html)
+            for address, port in re_ip_address:
+                result = address+':'+ port
+                yield result.replace(' ', '')
+    
+    def crawl_kuaidaili(self):
+        for i in range(1, 4):
+            start_url = 'http://www.kuaidaili.com/free/inha/{}/'.format(i)
+            html = get_page(start_url)
+            if html:
+                ip_address = re.compile('<td data-title="IP">(.*?)</td>') 
+                re_ip_address = ip_address.findall(html)
+                port = re.compile('<td data-title="PORT">(.*?)</td>')
+                re_port = port.findall(html)
+                for address,port in zip(re_ip_address, re_port):
+                    address_port = address+':'+port
+                    yield address_port.replace(' ','')
 
 
             
