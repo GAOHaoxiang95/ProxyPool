0d8e663dea6262c9b06ea63a0c8bb53b92fa88b8
Haoxiang Gao <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-04-15 14:07:20 +0800

    Update crawler.py

--- proxypool/crawler.py
+++ proxypool/crawler.py
@@ -24,24 +24,7 @@ class Crawler(object, metaclass=ProxyMetaclass):
             proxies.append(proxy)
         return proxies
        
-    def crawl_daili66(self, page_count=4):
-        """
-        获取代理66
-        :param page_count: 页码
-        :return: 代理
-        """
-        start_url = 'http://www.66ip.cn/{}.html'
-        urls = [start_url.format(page) for page in range(1, page_count + 1)]
-        for url in urls:
-            print('Crawling', url)
-            html = get_page(url)
-            if html:
-                doc = pq(html)
-                trs = doc('.containerbox table tr:gt(0)').items()
-                for tr in trs:
-                    ip = tr.find('td:nth-child(1)').text()
-                    port = tr.find('td:nth-child(2)').text()
-                    yield ':'.join([ip, port])
+
                     
     def crawl_kuaidaili(self):
         for i in range(1, 4):
